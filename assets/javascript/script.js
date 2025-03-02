6aac0923c08ec77eb03fd4d7852f49f64c4af138
kiara <fauzanarjuna996@gmail.com>
2025-03-02 13:53:08 +0700

    update 2 maret siang

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -185,6 +185,54 @@ modalElement3.addEventListener('hidden.bs.modal', function () {
         }, 500);
     }
 );
+
+var modalElement4 = document.getElementById('exampleModal4');
+var videoContainer4 = document.querySelector('.video-container4');
+var videoIframe4 = document.getElementById('videomodal4');
+
+modalElement4.addEventListener('hidden.bs.modal', function () {
+    videoIframe4.remove();
+        setTimeout(function() {
+            videoContainer4.innerHTML = '<iframe id="videomodal4" width="100%" height="393.75" src="https://www.youtube.com/embed/BcmKOYfvPEI?si=tQVNZ8NdtJ_CC9dw" title="Switch" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>';
+        }, 500);
+    }
+);
+
+var modalElement5 = document.getElementById('exampleModal5');
+var videoContainer5 = document.querySelector('.video-container5');
+var videoIframe5 = document.getElementById('videomodal5');
+
+modalElement5.addEventListener('hidden.bs.modal', function () {
+    videoIframe5.remove();
+        setTimeout(function() {
+            videoContainer5.innerHTML = '<iframe id="videomodal5" width="100%" height="500" src="https://jasminesmada.com" title="Switch" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>';
+        }, 500);
+    }
+);
+
+var modalElement6 = document.getElementById('exampleModal6');
+var videoContainer6 = document.querySelector('.video-container6');
+var videoIframe6 = document.getElementById('videomodal6');
+
+modalElement6.addEventListener('hidden.bs.modal', function () {
+    videoIframe6.remove();
+        setTimeout(function() {
+            videoContainer6.innerHTML = '<iframe id="videomodal6" width="100%" height="500" src="https://redjasminesmada.my.id" title="Switch" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>';
+        }, 500);
+    }
+);
+
+var modalElement7 = document.getElementById('exampleModal7');
+var videoContainer7 = document.querySelector('.video-container7');
+var videoIframe7 = document.getElementById('videomodal7');
+
+modalElement7.addEventListener('hidden.bs.modal', function () {
+    videoIframe7.remove();
+        setTimeout(function() {
+            videoContainer7.innerHTML = '<iframe id="videomodal7" width="100%" height="500" src="https://jasminesmada-news.blogspot.com/" title="Switch" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>';
+        }, 500);
+    }
+);
 /*------------------------------------- Testimonial Slider -------------------------------------*/
 $(document).ready(function () {
     $('.testimonial').slick({
